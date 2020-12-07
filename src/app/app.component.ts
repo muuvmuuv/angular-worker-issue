cc91dbe913ed577f36a3ddbe8875bbf01023bc2a
Marvin Heilemann <marvin.heilemann@googlemail.com>
2020-12-07 15:01:46 +0100

    exclude json module import as error source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { fromWorker } from 'observable-webworker';
 import { of } from 'rxjs';
 
+import data from './data.json'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,7 +15,11 @@ export class AppComponent implements OnInit {
   varOne: string
   varTwo: number
 
+  data = data
+
   ngOnInit(): void {
+    console.log(this.data);
+
     fromWorker<number, string>(
       () => new Worker('./workers/one.worker.ts', { type: 'module' }),
       of(91528391)
